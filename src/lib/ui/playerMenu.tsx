74628182d05481e9999dbbdf770206f3193bd362
Adam Sparks <adamsparks92@gmail.com>
2024-12-13 18:51:02 -0800

    fix: allow expanding player menus

--- src/lib/ui/playerMenu.tsx
+++ src/lib/ui/playerMenu.tsx
@@ -42,7 +42,7 @@ export const LocalPlayer: Component = props => {
     <Card>
       <div class='p-2 rounded-lg shadow-md w-64 flex-shrink-0' style='pointer-events: initial'>
         <Collapsible onOpenChange={setOpen} open={open()}>
-          <div class='flex gap-5 items-center justify-around'>
+          <div class='flex gap-5 items-center justify-between'>
             You
             <NumberField
               style='width: 6rem'
@@ -60,7 +60,7 @@ export const LocalPlayer: Component = props => {
                 <NumberFieldDecrementTrigger />
               </div>
             </NumberField>
-            <CollapsibleTrigger class='size-4'>
+            <CollapsibleTrigger class='size-6'>
               <ChevronDownIcon
                 style={`transform: rotate3d(1,0,0,${
                   open() ? 180 : 0
@@ -97,7 +97,7 @@ export const LocalPlayer: Component = props => {
           <CollapsibleContent>
             <hr class='my-3' />
             <DropdownMenu>
-              <DropdownMenuTrigger>Add Counters</DropdownMenuTrigger>
+              <DropdownMenuTrigger class='mb-3'>Add Counters</DropdownMenuTrigger>
               <DropdownMenuContent>
                 <For each={counters()}>
                   {counter => {
@@ -111,7 +111,6 @@ export const LocalPlayer: Component = props => {
                             defaultValue={props?.counters?.[counter.id] ?? 0}
                             style='width: 6rem'
                             onChange={rawValue => {
-                              let localState = provider.awareness.getLocalState();
                               let value = parseInt(rawValue, 10);
                               if (isNaN(value)) value = 0;
                               changeCounter(counter.id, value);
@@ -183,13 +182,15 @@ export const NetworkPlayer: Component = props => {
     <Card>
       <div class='rounded-lg shadow-md w-64 flex-shrink-0 p-2' style='pointer-events: initial'>
         <Collapsible onOpenChange={setOpen} open={open()}>
-          <div class='flex gap-5 items-center justify-around' style='min-height: 40px;'>
+          <div class='flex gap-5 items-center justify-between' style='min-height: 40px;'>
             {props?.name}
             <div>{props?.life}</div>
-            <CollapsibleTrigger>
-              <svg height='1rem' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
-                <path d='M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z' />
-              </svg>
+            <CollapsibleTrigger class='size-6'>
+              <ChevronDownIcon
+                style={`transform: rotate3d(1,0,0,${
+                  open() ? 180 : 0
+                }deg); transition: transform 250ms ease-in-out;`}
+              />
             </CollapsibleTrigger>
           </div>
           <Show when={!open()}>
@@ -223,13 +224,12 @@ export const NetworkPlayer: Component = props => {
             <ul class='space-y-2'>
               <For
                 each={Object.entries(props?.counters ?? {})
-                  // .filter(entry => entry[1] !== 0)
                   .map(entry => entry[0])
                   .sort((a, b) => a.localeCompare(b))}>
                 {counterId => {
                   let counter = counters().find(c => c.id === counterId);
                   return (
-                    <li class='flex justify-between text-gray-600'>
+                    <li class='flex justify-between items-center'>
                       <span style='text-align: start;'>{counter.name}</span>
                       <span class='font-semibold'>
                         <NumberField
